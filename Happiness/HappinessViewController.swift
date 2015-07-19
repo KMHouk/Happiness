0367075fdca2a479487dcf7e4d564405ac448a8a
Kevin Houk <kmhouk@gmail.com>
2015-07-19 18:08:16 -0400

    first commit

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Kevin on 7/19/15.
+//  Copyright (c) 2015 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
